6e8b25d757f94f920f92a36e79a38d61ea2669dd
Marco Boucas <marco.boucas@hotmail.fr>
2021-11-29 18:32:13 +0100

    fix linting problems

--- src/models/object_detector.py
+++ src/models/object_detector.py
@@ -8,6 +8,7 @@ import pandas as pd
 import torch
 
 
+# pylint: disable=too-few-public-methods
 class ObjectDetector:
     """Object detector using YOLO."""
 
@@ -15,19 +16,18 @@ class ObjectDetector:
         self, model_name: str = "ultralytics/yolov5", model_version: str = "yolov5s"
     ) -> None:
         """Init."""
-        self.model = torch.hub.load(
-            repo_or_dir=model_name, model=model_version, pretrained=True
-        )
-        logging.info(f"[Object Detector] Now ready to operate!")
+        self.model = torch.hub.load(repo_or_dir=model_name, model=model_version, pretrained=True)
+        logging.info("[Object Detector] Now ready to operate!")
 
     def extract_objects(self, images: List[str]) -> pd.DataFrame:
         """Extract objects.
 
         :param images: A list of images pathes.
 
-        :returns: All the boxes (as dataframe), columns: img_name,x1,x2,y1,y2,yolo_confidence,yolo_class_id,yolo_class_name.
+        :returns: All the boxes (as dataframe),
+        columns: img_name,x1,x2,y1,y2,yolo_confidence,yolo_class_id,yolo_class_name.
         """
-        logging.info(f"[Object Detector] Prediction on {len(images)} images")
+        logging.info("[Object Detector] Prediction on %i images", len(images))
         results = self.model(images[:])
         final_results = []
         for image_path, result in zip(images, results.pandas().xyxy):
@@ -56,13 +56,8 @@ class ObjectDetector:
 
 
 if __name__ == "__main__":
-    import os
-
     model = ObjectDetector()
-    results = model.extract_objects(
-        [
-            os.path.join("data", "train", "images", str(i).rjust(4, "0") + ".jpg")
-            for i in range(1)
-        ]
+    _results = model.extract_objects(
+        [os.path.join("data", "train", "images", str(i).rjust(4, "0") + ".jpg") for i in range(1)]
     )
-    print(results.head(10))
+    print(_results.head(10))
